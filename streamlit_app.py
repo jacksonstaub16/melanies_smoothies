daef3eac73d12e443c14047d66c19643a9c96bd2
jacksonstaub16 <31611940+jacksonstaub16@users.noreply.github.com>
2024-05-22 10:36:47 +0100

    Update streamlit_app.py
    Connect changes needed bc of move srom SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-
 
 # Write directly to the app
 st.title("Customize Your Smoothie! :cup_with_straw:")
@@ -12,7 +10,8 @@ st.write("Choose the fruits you want in your custom Smoothie!"
 name_on_order = st.text_input("Name on Smoothie")
 st.write("Name on smoothie will be:", name_on_order)
 
-session = get_active_session()
+cnx =st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
 ingredients_list = st.multiselect(
